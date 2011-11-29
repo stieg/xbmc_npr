0d9f02604980aa77750c830c990dece142fb2536
Andrew Stiegmann (stieg) <andrew.stiegmann@gmail.com>
2011-11-28 23:10:23 -0800

    Add support for KQED.
    Refactored some of the code so I don't go insane reading it.
    
    Tested on Pre-release Eden (12/22)
    
    Add 2.0 logic

--- plugin.audio.npr/default.py
+++ plugin.audio.npr/default.py
@@ -1,6 +1,3 @@
-#/*
-# *      Copyright (C) Fisslefink
-# *
 # *
 # *  This Program is free software; you can redistribute it and/or modify
 # *  it under the terms of the GNU General Public License as published by
@@ -17,75 +14,81 @@
 # *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 # *  http://www.gnu.org/copyleft/gpl.html
 # *
-# */
 
-import string,xbmc,xbmcaddon,xbmcplugin,xbmcgui
+
+import xbmc,xbmcaddon,xbmcplugin,xbmcgui,sys
 
 __XBMC_Revision__ = xbmc.getInfoLabel('System.BuildVersion')
 __settings__      = xbmcaddon.Addon(id='plugin.audio.npr')
 __language__      = __settings__.getLocalizedString
 __version__       = __settings__.getAddonInfo('version')
 __cwd__           = __settings__.getAddonInfo('path')
-__addonname__    = "NPR"
-__addonid__      = "plugin.audio.npr"
-__author__        = "Fisslefink"
+__addonname__     = "NPR - National Public Radio"
+__addonid__       = "plugin.audio.npr"
+__author__        = "Stieg,Fisslefink"
 
 
 def PLAY(url):
   xbmc.Player().play(url)
-                  
+
 def get_params():
-        param=[]
-        paramstring=sys.argv[2]
-        if len(paramstring)>=2:
-                params=sys.argv[2]
-                cleanedparams=params.replace('?','')
-                if (params[len(params)-1]=='/'):
-                        params=params[0:len(params)-2]
-                pairsofparams=cleanedparams.split('&')
-                param={}
-                for i in range(len(pairsofparams)):
-                        splitparams={}
-                        splitparams=pairsofparams[i].split('=')
-                        if (len(splitparams))==2:
-                                param[splitparams[0]]=splitparams[1]
-                                
+        param = []
+        paramstring = sys.argv[2]
+        if len(paramstring) >= 2:
+                params = sys.argv[2]
+                cleanedparams = params.replace('?','')
+                if (params[-1] == '/'):
+                        params = params[0:-2]
+                pairsofparams = cleanedparams.split('&')
+                param = {}
+                for l in pairsofparams:
+                        splitparams = l.split('=')
+                        if len(splitparams) == 2:
+                                param[splitparams[0]] = splitparams[1]
+
         return param
 
 
-params=get_params()
-stream=None
+params = get_params()
+stream = None
 
 try:
-        stream=int(params["stream"])
+        stream = int(params["stream"])
 except:
         pass
 try:
-        firstrun=(int(sys.argv[1]) == 0)
+        firstrun = (int(sys.argv[1]) == 0)
 except:
         pass
 
-print "Selected stream: "+str(stream)
+print "Selected stream: " + str(stream)
 
-if stream==None:
-	u=sys.argv[0]+"?stream=1"
-	liz=xbmcgui.ListItem(__language__(30090), iconImage="DefaultPlaylist.png", thumbnailImage="")
-	xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+if stream == None:
+        u = sys.argv[0]+"?stream=1"
+        liz = xbmcgui.ListItem(__language__(30090), iconImage="DefaultPlaylist.png", thumbnailImage="")
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
 
-	u=sys.argv[0]+"?stream=2"
-	liz=xbmcgui.ListItem(__language__(30091), iconImage="DefaultPlaylist.png", thumbnailImage="")
-	xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        u = sys.argv[0]+"?stream=2"
+        liz = xbmcgui.ListItem(__language__(30091), iconImage="DefaultPlaylist.png", thumbnailImage="")
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
 
-	xbmcplugin.endOfDirectory(int(sys.argv[1]))
-       
-elif stream==1:
+        u = sys.argv[0]+"?stream=3"
+        liz = xbmcgui.ListItem(__language__(30092), iconImage="DefaultPlaylist.png", thumbnailImage="")
+        xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+elif stream == 1:
         print "Playing stream 1"
-	url='http://npr.ic.llnwd.net/stream/npr_live24'
-        PLAY(url)
-        
-elif stream==2:
+        _url = 'http://npr.ic.llnwd.net/stream/npr_live24'
+        PLAY(_url)
+
+elif stream == 2:
         print "Playing stream 2"
-	url='http://sc9.sjc.llnw.net:80/stream/npr_music2'
-        PLAY(url)
+        _url = 'http://sc9.sjc.llnw.net:80/stream/npr_music2'
+        PLAY(_url)
 
-
+elif stream == 3:
+        print "Playing stream 3"
+        _url = 'http://www.kqed.org/radio/listen/kqedradio.pls'
+        PLAY(_url)
